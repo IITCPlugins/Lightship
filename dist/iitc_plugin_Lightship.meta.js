d93b06ee9cd0923e590349a79faf9a9c725ead93
McBen <viertelvor12@gmx.net>
2023-06-09 11:46:23 +0200

    build: Add update url

---
+++ dist/iitc_plugin_Lightship.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name            IITC plugin: Lightship
+// @id              iitc_plugin_Lightship
+// @category        Highlighter
+// @version         1.0
+// @namespace       https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL       https://github.com/IITCPlugins/Lightship/raw/main/dist/iitc_plugin_Lightship.meta.js
+// @downloadURL     https://github.com/IITCPlugins/Lightship/raw/main/dist/iitc_plugin_Lightship.user.js
+// @description     Highlight Overclock portals
+// @match           https://intel.ingress.com/*
+// @author          McBen
+// ==/UserScript==
